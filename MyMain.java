344435d302142a03be698c7757b71aaf02f84229
Deepak Yadav <deepakyadav1331@gmail.com>
2026-06-19 15:58:04 +0530

    Fix syntax error in MyMain.java

--- MyMain.java
+++ MyMain.java
@@ -1,5 +1,5 @@
 public class MyMain {
     public static void main(String[] args) {
-        System.out.println("Hello World from Jenkins. Version 1.2"
+        System.out.println("Hello World from Jenkins. Version 1.2")
     }
 }
